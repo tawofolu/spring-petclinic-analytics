b400758c64fb8a9c3c36800c999b42b4d14db6e0
Tola Awofolu <oawofolu@pivotal.io>
2021-07-28 15:32:04 -0400

    Merge

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 PETCLINIC_ANALYTICS_IMAGE_VERSION=`date "+%Y%m%d.%H%M"`
-docker build -t oawofolu/petclinic-analytics:0.30 .
-docker tag oawofolu/petclinic-analytics:0.30 oawofolu/petclinic-analytics
-docker push oawofolu/petclinic-analytics:0.30
-docker push oawofolu/petclinic-analytics:0.30
+docker build -t oawofolu/petclinic-analytics .
+docker push oawofolu/petclinic-analytics
+docker push oawofolu/petclinic-analytics
 kubectl delete svc petclinic-app-analytics --ignore-not-found=true
 kubectl delete deploy petclinic-app-analytics --ignore-not-found=true
-kubectl create deployment petclinic-app-analytics --image=oawofolu/petclinic-analytics:0.30
+kubectl create deployment petclinic-app-analytics --image=oawofolu/petclinic-analytics
 #kubectl port-forward deploy/petclinic-app-analytics 8050
